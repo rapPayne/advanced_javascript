4703e13b6ad9fbf4e40af2eaba6aa2a4e4b448f5
rapPayne <bob@creator.net>
2020-04-24 15:32:40 -0500

    Added starter for 14 Ajax

---
+++ starters/14_ajax/people.js
@@ -0,0 +1,28 @@
+
+
+/*
+  return `
+  <div class="peopleCard mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand" style="background-image: url(${picture.large})">
+    <h2 class="mdl-card__title-text">${first} ${last}</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+    <table>
+      <tbody>
+      <tr><th>email:</th><td>${email}</td></tr>
+      <tr><th>cell:</th><td>${cell}</td></tr>
+      <tr><th>address:</th><td>${number} ${name}<br />${city}, ${state} ${postcode}<br />${country}</td></tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      Update
+    </a>
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+      Contact
+    </a>
+  </div>
+  </div>
+  `;
+*/
