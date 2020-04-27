fbf88eb7db18201f3282c5366e7295316e173c42
rapPayne <bob@creator.net>
2020-04-27 10:09:05 -0500

    Small refactor to match instructions

--- solutions/03_dom/calculator.js
+++ solutions/03_dom/calculator.js
@@ -2,37 +2,39 @@ let firstValue = 0;
 let operator = "";
 const inputBox = document.querySelector("input");
 document.querySelectorAll('button')
-  .forEach(btn => btn.addEventListener("click", e => {
-    switch (btn.textContent) {
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-      case "0":
-      case ".":
-        appendCharacter(btn.textContent);
-        break;
-      case "+":
-      case "-":
-      case "x":
-      case "/":
-        symbol(btn.textContent);
-        break;
-      case "=":
-        equals();
-        break;
-      case "c":
-        clearInput();
-        break;
-      default:
-        console.warn(`The ${btn.textContent} key isn't supported`);
-    }
-  }));
+  .forEach(btn => btn.addEventListener("click", e => readButton(btn)));
+
+function readButton(btn) {
+  switch (btn.textContent) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+    case ".":
+      appendCharacter(btn.textContent);
+      break;
+    case "+":
+    case "-":
+    case "x":
+    case "/":
+      symbol(btn.textContent);
+      break;
+    case "=":
+      equals();
+      break;
+    case "c":
+      clearInput();
+      break;
+    default:
+      console.warn(`The ${btn.textContent} key isn't supported`);
+  }
+}
 
 function appendCharacter(character) {
   inputBox.value = inputBox.value + character;
